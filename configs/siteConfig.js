3d53098fc6a5624bcdebdee0323265e3adcdd4be
Blue <13604413+Bluethefurry@users.noreply.github.com>
2020-03-03 15:48:40 +0100

    Update siteConfig.js

--- configs/siteConfig.js
+++ configs/siteConfig.js
@@ -95,10 +95,10 @@ const siteConfig = {
    docsSideNavCollapsible: true,
 
   // Show documentation's last contributor's name.
-   enableUpdateBy: true,
+  //enableUpdateBy: true,
 
   // Show documentation's last update time.
-  enableUpdateTime: true,
+  // enableUpdateTime: true,
 
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
