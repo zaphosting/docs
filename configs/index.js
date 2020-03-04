450d56341b17f8806b886329d76e17cde643de46
Blue <13604413+Bluethefurry@users.noreply.github.com>
2020-03-04 02:21:33 +0100

    make index links functional

--- configs/index.js
+++ configs/index.js
@@ -64,9 +64,9 @@ class HomeSplash extends React.Component {
         <div className="inner">
           <ProjectTitle tagline={siteConfig.tagline} title={siteConfig.title} />
           <PromoSection>
-            <Button href="#try">Try It Out</Button>
-            <Button href={docUrl('doc1.html')}>Example Link</Button>
-            <Button href={docUrl('doc2.html')}>Example Link 2</Button>
+            <Button href={docUrl('firststeps_register.html')}>Register an Account</Button>
+            <Button href={docUrl('firststeps_rent.html')}>Rent a Server</Button>
+            <Button href={docUrl('firststeps_rent_contractorprepaid.html')}>Contract or Prepaid?</Button>
           </PromoSection>
         </div>
       </SplashContainer>
