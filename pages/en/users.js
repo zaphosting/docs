3d97cc314206aef6a394045a3cdd9dd20667edf9
Blue <13604413+Bluethefurry@users.noreply.github.com>
2020-03-09 14:28:34 +0100

    Oops fix (#67)
    * Revert "build fail fix (#66)"
    
    This reverts commit 62e2d6e94b316c2ea91c21fe3257533090b8d9cb.
    
    * disable user page

--- pages/en/users.js
+++ pages/en/users.js
@@ -3,7 +3,10 @@
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
- */
+
+
+
+ disabled as we currently dont have defined users.
 
 const React = require('react');
 
@@ -46,3 +49,5 @@ class Users extends React.Component {
 }
 
 module.exports = Users;
+
+ */
