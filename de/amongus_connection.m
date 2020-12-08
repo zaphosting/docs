d91b1ad62cbd0986d1288468855911473d16c1d8
sM00thCF <61839701+sM00thCF@users.noreply.github.com>
2020-12-08 13:38:27 +0100

    amongus_connection.md

---
+++ de/amongus_connection.m
@@ -0,0 +1,62 @@
+---
+id: amongus_connection
+title: Verbindung 
+sidebar_label: Verbindung zum Server
+---
+
+## Vorbereitung - Download Impostor
+
+Damit du dich mit deinem Impostor - Among Us Server verbinden kannst, musst du vorab hier die Imposter Patcher Client Files herunterladen:
+[Impostor](https://ci.appveyor.com/project/Impostor/Impostor/branch/dev/artifacts)
+
+Wähle hierbei die Daten aus, welche deinem Betriebssystem entsprechen:
+![](https://screensaver01.zap-hosting.com/index.php/s/rMFgTyzF7wxnjxN/preview)
+
+Hast du zum Beispiel Windows installiert, kannst du diese Datei herunterladen:
+![](https://screensaver01.zap-hosting.com/index.php/s/9LMR43y8GprHzes/preview)
+
+Du kannst das Archiv nun in einem von dir gewünschten Ordner auf deinem PC kopieren und das Archiv dort entpacken.
+Zum entpacken machst du einen Rechtsklick auf die Datei und wählst dann "hier entpacken" aus.
+Das sieht dann so aus:
+
+![](https://screensaver01.zap-hosting.com/index.php/s/tGMwEqcqa9y4BC6/preview)
+
+## Konfiguration
+Mache nun einen Dopelklick auf die Impostor.exe Datei.
+Es öffnet sich nun das folgende Fenster:
+
+![](https://screensaver01.zap-hosting.com/index.php/s/Ws8Dpgj7JFFJo5K/preview)
+
+Trage dort die IP und den Port von deinem Server ein.
+In unserem Beispiel wären es die folgenden Daten:
+92.42.46.124:22122
+
+Klicke nun auf "Save".
+
+## Server ingame "erstellen"
+
+Starte nun Among Us auf deinem PC.
+Klicke im Hauptmenü auf "online" und klicke dann auf "Create Game".
+
+![](https://screensaver01.zap-hosting.com/index.php/s/9GatqyTk3REn2qF/preview)
+
+Hier kannst du nun die gewünschten Einstellungen für deinen Server vornehmen:
+
+![](https://screensaver01.zap-hosting.com/index.php/s/oifZ4Nx9FkwR6d2/preview)
+
+Soweit du fertig bist mit den Einstellungen kannst du auf "confirm" klicken.
+Du bist nun mit deinem Server verbunden!
+
+In der Lobby kannst du nun einstellen, ob es ein öffentlicher oder ein privater Server sein soll.
+Zum umstellen kannst du dafür auf das "Private" bzw "Public" Icon klicken:
+
+![](https://screensaver01.zap-hosting.com/index.php/s/WecjHwipkQyyx5X/preview)
+
+Steht der Server auf "Public" können andere Spieler auf die selbe Art und Weise sich mit dem Server verbinden.
+Diese müssen dann im Hauptmenü im Spiel nicht auf "Create Game" sondern auf "Find Game" klicken.
+Hier ist es wichtig, dass der Chat auf "others" steht und dass die Filter genau den Einstellungen vom Server entsprechen.
+Ansonsten wird der Server nicht angezeigt.
+
+Handelt es sich um einen "Private" Server muss man im Hauptmenü auf "Enter Code" klicken.
+Hier gibt man den Code an, welchen der Ersteller vom Server in der Lobby einsehen kann.
+
