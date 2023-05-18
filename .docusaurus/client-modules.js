5c9ef872b7f788063e3e88eae56e59aa79983b18
Blumlaut <13604413+Blumlaut@users.noreply.github.com>
2023-05-18 17:25:53 +0200

    remove docusaurus files

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/home/github-runners/runner2/_work/docs/docs/docusaurus/node_modules/infima/dist/css/default/default.css'),
-  require('/home/github-runners/runner2/_work/docs/docs/docusaurus/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/home/github-runners/runner2/_work/docs/docs/docusaurus/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/home/github-runners/runner2/_work/docs/docs/docusaurus/src/css/custom.css'),
-];
