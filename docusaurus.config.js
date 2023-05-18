04a39486a281e1a2afa7fe2e97c730ee1ba9a113
Blumlaut <13604413+Blumlaut@users.noreply.github.com>
2023-05-18 20:26:56 +0200

    enable editLocalizedFiles

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,6 +43,7 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           editUrl: 'https://github.com/zaphosting/docs/tree/master/',
+          editLocalizedFiles: true
         },
         blog: false,
         theme: {
