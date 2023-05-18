54e1508914ccf6a0ae43553117740767ceeb06d7
Blumlaut <13604413+Blumlaut@users.noreply.github.com>
2023-05-18 15:39:20 +0200

    this will need to be changed later

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,8 +17,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/docs/firststeps_register">
+            Open Docs
           </Link>
         </div>
       </div>
