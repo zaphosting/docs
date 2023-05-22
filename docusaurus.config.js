988c7d76fb1d46e47a7f50726a240d963432fa62
Blumlaut <13604413+Blumlaut@users.noreply.github.com>
2023-05-22 13:04:43 +0200

    disable social card for now

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -57,7 +57,8 @@ const config = {
   /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
   ({
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    //image: 'img/docusaurus-social-card.jpg',
+    image: undefined,
     navbar: {
       title: 'ZAP-Hosting Docs',
       logo: {
