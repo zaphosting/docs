97af7732b15c1d7ee7774de4fe612f54520b2f67
Florian Galz <florian.galz@outlook.com>
2024-05-02 17:00:42 +0200

    Revert "Update docusaurus.config.js (#1229)"
    This reverts commit 1748eebf5307af0dfac1823c03634a236bf31b71.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,6 @@ const config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/guides/',
-  trailingSlash: false,
   
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
