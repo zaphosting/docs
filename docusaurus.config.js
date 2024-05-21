92aa56ebf60f7b4399aafac437c6f96dbe602698
zapaul <nothankyou@nothankyouverymuch.cheers>
2024-05-21 14:06:20 +0000

    Offer coupon below TOC in sidebar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,6 +27,10 @@ const config = {
   onBrokenMarkdownLinks: 'throw',
   onDuplicateRoutes: 'throw',
   onBrokenAnchors: 'throw',
+
+  customFields: {
+    marketingSite: 'https://zap-hosting.com',
+  },
   
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
