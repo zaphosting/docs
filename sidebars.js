0e031cc1e6b601cc05beb9537e26b0552684f038
fgalz <florian.galz@outlook.com>
2025-03-03 18:41:10 +0100

    New guide: Half-Life 1 - Connect to the server

--- sidebars.js
+++ sidebars.js
@@ -1177,6 +1177,7 @@ const sidebars = {
           label: "First Steps",
           items: [
             "hl1-firststeps-dashboard",
+            "hl1-firststeps-connect",
           ]
         },
       ],
