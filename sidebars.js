ed2cf42a5d38c5e97afeda50ceeaeb0c4cf91a22
fgalz <florian.galz@outlook.com>
2025-03-26 18:14:04 +0100

    Barotrauma

--- sidebars.js
+++ sidebars.js
@@ -667,6 +667,7 @@ const sidebars = {
           label: "First Steps",
           items: [
             "barotrauma-firststeps-dashboard",
+            "barotrauma-firststeps-connect",
           ]
         },
         "barotrauma-becomeadmin"
