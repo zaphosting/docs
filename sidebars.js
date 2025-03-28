eb7b60abd16ddf7b46826b30e1d8caed72795666
fgalz <florian.galz@outlook.com>
2025-03-28 15:19:10 +0100

    Portal Knights

--- sidebars.js
+++ sidebars.js
@@ -1775,6 +1775,7 @@ const sidebars = {
           label: "First Steps",
           items: [
             "portalknights-firststeps-dashboard",
+            "portalknights-firststeps-connect",
           ]
         },
       ],
