db8b7709aac5069897234f04300e42403352ac5c
fgalz <florian.galz@outlook.com>
2026-02-28 14:18:21 +0100

    YouTube Redesign

--- src/components/YouTube/YouTube.js
+++ src/components/YouTube/YouTube.js
@@ -2,26 +2,42 @@ import React from 'react';
 import styles from './YouTube.module.css';
 
 const YouTube = ({ videoId, imageSrc, title, description }) => {
-  const handleContentClick = () => {
-    const videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
-    window.open(videoUrl, '_blank');
+  const handleClick = () => {
+    window.open(`https://www.youtube.com/watch?v=${videoId}`, '_blank');
   };
 
   return (
-    <div className={styles.YouTubeContainer} onClick={handleContentClick}>
-      <div className={styles.videoContainer}>
-        <img
-          className={styles.videoImage}
-          src={imageSrc}
-          alt={title}
-        />
-        <div className={styles.overlay}>
-          <div className={styles.playIcon}></div>
+    <div className={styles.container} onClick={handleClick}>
+      <div className={styles.thumbnail}>
+        <img className={styles.image} src={imageSrc} alt={title} data-no-zoom />
+        <div className={styles.imageOverlay} />
+
+        <div className={styles.playButton}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={styles.playIcon}>
+            <path fillRule="evenodd" d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z" clipRule="evenodd" />
+          </svg>
+        </div>
+
+        <div className={styles.badge}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={styles.badgeIcon}>
+            <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm14.024-.983a1.125 1.125 0 0 1 0 1.966l-5.603 3.113A1.125 1.125 0 0 1 9 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113Z" clipRule="evenodd" />
+          </svg>
+          YouTube
         </div>
       </div>
-      <div className={styles.contentContainer}>
-        <p className={styles.title}>{title}</p>
-        <p className={styles.description}>{description}</p>
+
+      <div className={styles.content}>
+        <div className={styles.contentInner}>
+          <p className={styles.title}>{title}</p>
+          {description && <p className={styles.description}>{description}</p>}
+        </div>
+
+        <div className={styles.cta}>
+          <span className={styles.ctaText}>Watch video</span>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className={styles.ctaArrow}>
+            <path fillRule="evenodd" d="M5.22 14.78a.75.75 0 0 0 1.06 0l7.22-7.22v5.69a.75.75 0 0 0 1.5 0v-7.5a.75.75 0 0 0-.75-.75h-7.5a.75.75 0 0 0 0 1.5h5.69l-7.22 7.22a.75.75 0 0 0 0 1.06Z" clipRule="evenodd" />
+          </svg>
+        </div>
       </div>
     </div>
   );
