af80e8f6e1a75ea8f6c9e5d2489ce91833601d1e
fgalz <florian.galz@outlook.com>
2026-04-22 14:10:23 +0200

    Optimize Docusaurus build workflow time

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -104,7 +104,7 @@ const config = {
     [
       '@docusaurus/plugin-pwa',
       {
-        debug: true,
+        debug: false,
         offlineModeActivationStrategies: [
           'appInstalled',
           'standalone',
